e4214bc9ec74b65c7e1a79185876cbd9454136c0
unknown <dhananjanarathnayake123@gmail.com>
2024-08-11 01:58:44 +0530

    Data collection completed

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version = '0.0.0',
+    author = 'Muditha',
+    author_email = 'dhananjanarathnayake123@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+    
+)
