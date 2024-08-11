5d6f232d6e25c7a52b5edfcbb55cab9b71781e80
unknown <dhananjanarathnayake123@gmail.com>
2024-08-11 23:02:29 +0530

    zip file removed temporily

--- app.py
+++ app.py
@@ -1,3 +1,10 @@
 from signLanguage.logger import logging
+from signLanguage.exception import SignException
+import sys
 
-logging.info("Testing the logger")
+#logging.info("Testing the logger")
+
+try:
+    a = 6/'8'
+except Exception as e:
+    raise SignException(e,sys)
